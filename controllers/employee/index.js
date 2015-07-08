9cff395eafe42852c92cc74b38da5d751c040e47
DirkViljoen <dirkcharl.viljoen@gmail.com>
2015-07-09 00:26:16 +0200

    feat(Employee) Added employee view, search, update

--- controllers/employee/index.js
+++ controllers/employee/index.js
@@ -11,20 +11,44 @@ module.exports = function (router) {
 
     router.get('/', function (req, res) {
         //You can find me at /employee
-        res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+        //res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+        res.render('employee', model)
     });
 
     router.get('/add', function (req, res) {
-      console.log('test add');
         //You can find me at /employee/add
         //res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
         res.render('employee-add', model)
     });
 
     router.get('/add-image', function (req, res) {
-      console.log('test add');
         //You can find me at /employee/add-image
         //res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
         res.render('employee-add-image', model)
     });
+
+    router.get('/view', function (req, res) {
+        //You can find me at /employee/add-image
+        //res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+        res.render('employee-view', model)
+    });
+
+    router.get('/view-image', function (req, res) {
+        //You can find me at /employee/add-image
+        //res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+        res.render('employee-view-image', model)
+    });
+
+    router.get('/update', function (req, res) {
+        //You can find me at /employee/add-image
+        //res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+        res.render('employee-update', model)
+    });
+
+    router.get('/update-image', function (req, res) {
+        //You can find me at /employee/add-image
+        //res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+        res.render('employee-update-image', model)
+    });
+
 };
--- models/employee.js
+++ models/employee.js
@@ -3,6 +3,23 @@
 
 module.exports = function EmployeeModel() {
     return {
-        name: 'employee'
+        empFName: 'John',
+        empLName: 'Doe',
+        empCNumber: '082 453 4532',
+        empSalary: '10 000',
+
+        empUsername: 'johdoe',
+        empPassword: 'asdfg',
+
+        empEmail: 'johdoe@esalon.co.za',
+        empALine1: '43 Oaktree Avenue',
+        empALine2: '',
+        empSuburb: '',
+        empCity: '',
+        empProvince: '',
+
+        empRole: 'Stylist',
+
+        empImage: 'https://www.google.co.za/images/nav_logo195.png'
     };
 };
