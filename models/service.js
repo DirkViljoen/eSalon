dff32e2bc1aef9321f3eca559bdac9fc001aa256
DirkViljoen <dirkcharl.viljoen@gmail.com>
2015-07-09 11:32:56 +0200

    refactor(Services) implement combobox mixin

--- models/service.js
+++ models/service.js
@@ -7,7 +7,51 @@ module.exports = function ServiceModel() {
         serAddInfo: 'none',
         serDuration: '30 minutes',
         serPrice: '100',
-        serCategory: 'Haircut',
-        serHairLength: 'Long'
+        serCategory: '1',
+        serHairLength: '3',
+        arrHairLength:
+          [
+            {
+              'id': '',
+              'value': 'Choose a hair length'
+            },
+            {
+              'id': '1',
+              'value': 'Short'
+            },
+            {
+              'id': '2',
+              'value': 'Medium'
+            },
+            {
+              'id': '3',
+              'value': 'Long'
+            }
+
+          ],
+        arrCategory:
+          [
+            {
+              'id': '',
+              'value': 'Choose a category'
+            },
+            {
+              'id': '1',
+              'value': 'Cutting'
+            },
+            {
+              'id': '2',
+              'value': 'Colouring'
+            },
+            {
+              'id': '3',
+              'value': 'Shampoos'
+            },
+            {
+              'id': '4',
+              'value': 'Other'
+            }
+
+          ]
     };
 };
