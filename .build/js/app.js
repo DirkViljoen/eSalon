b6cc01e24bcadd3f2743a66bfb0444c99949b8ff
DirkViljoen <dirkcharl.viljoen@gmail.com>
2015-07-14 10:14:32 +0200

    feat(DateTimePicker) Add date time picker functionality

--- .build/js/app.js
+++ .build/js/app.js
@@ -3,7 +3,11 @@ function createpicker(vID){
   var index;
   var a = document.getElementByID(vID);
   document.write(vID);
-
+  var pickers = [];
+  for (index = 0; index < a.length; ++index) {
+    pickers[pickers.length] = new Pikaday({ field:  a[index]});
+    console.log(a[index]);
+  };
 }
 
 
